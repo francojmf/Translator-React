727626a4c979951a088e2761d7b3cb62171c6625
Joao Franco <francojmf@users.noreply.github.com>
2022-10-10 22:52:53 -0300

    Delete api.key.js

--- pages/constants/api.key.js
+++
@@ -1,7 +0,0 @@
-const apiKey = "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";
-const organization = "DCTA";
-
-const _apiKey = apiKey;
-const _organization = organization;
-
-export { _apiKey as apiKey, _organization as organization};
